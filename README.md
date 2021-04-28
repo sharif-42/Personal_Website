9b895032284b727b7c05aa10506965080a8e317d
sharif_42 <sharifulcsehstu@gmail.com>
2021-04-28 22:26:52 +0600

    blog creation basic phase done

--- README.md
+++ README.md
@@ -1,7 +1,20 @@
 # Personal_Website
 
 ## Run the project in docker
-~~~~
+```shell
 docker-compose build
 docker-compose up
-~~~~
+```
+
+## Apply Migrations(run sequentially)
+```shell
+docker-compose run web sh 
+python manage.py makemigrations
+python manage.py migrate
+```
+
+## Create Super User
+```shell
+docker-compose run web sh
+python manage.py createsuperuser
+```
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views import View
+from django.views.generic import DetailView
 
-# Create your views here.
+from blog.models import Post
+
+
+class HomeView(View):
+    def get(self, *args, **kwargs):
+        posts = Post.objects.all()
+        context = {
+            'posts': posts,
+        }
+        return render(self.request, "home.html", context)
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post-detail.html'
